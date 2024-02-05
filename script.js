14b4c51a95fc392de67a0efdb328188ded242d43
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-02-06 02:03:48 +0500

    timer bug fixed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var level = "beginner";
 var operator = "Add";
+var time = 60
+var isPopupOpen = false
+var intervalId;
 var ans;
 var num1;
 var num2;
@@ -25,6 +28,9 @@ function changeOpr(e) {
 }
 
 function changeVal() {
+    document.getElementById("ans").value = ""
+    clearInterval(intervalId);
+    time = 20;
     if (level === "beginner" && operator == "Add" || operator == "Sub") {
         num2 = Math.ceil(Math.random() * 50)
         num1 = Math.ceil(Math.random() * 50) + num2
@@ -51,6 +57,15 @@ function changeVal() {
     }
     document.getElementById("num1").innerText = num1
     document.getElementById("num2").innerText = num2
+    if (!isPopupOpen) {
+        intervalId = setInterval(() => {
+            time = time - 1;
+            document.getElementById("timer").innerHTML = `00:${time}`;
+            if(time < 10){
+                document.getElementById("timer").innerHTML = `00:0${time}`;
+            }
+        }, 1000);
+    }
 }
 
 function submit(inp) {
@@ -60,20 +75,32 @@ function submit(inp) {
             "Correct",
             'Your answer is correct',
             'success'
-        ).then(()=>{
-            changeVal()
-        })
-        inp.previousElementSibling.value = ""
+            ).then(() => {
+                changeVal()
+            })
+            inp.previousElementSibling.value = ""
     } else {
         Swal.fire(
             "Wrong",
             `The answer you entered is wrong. the correct answer is ${eval(ans)}`,
             'error'
-        ).then(()=>{
-            changeVal()
+            ).then(() => {
+                changeVal()
         })
         inp.previousElementSibling.value = ""
     }
 }
+function checkTime() {
+    console.log(time);
+    if (time === 0 && !isPopupOpen) {
+        clearInterval(intervalId);
+        isPopupOpen = true; // Set the popup status to true
+        Swal.fire("Time is Over", "", "info").then(() => {
+            isPopupOpen = false; // Reset the popup status when the popup is closed
+            changeVal();
+        });
+    }
+}
 
+setInterval(checkTime, 1000)
 changeVal()
